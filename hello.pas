11f757d5e938b0768a517db268ba300c81aa5db0
strashilka123 <markleonov331@gmail.com>
2024-10-06 15:34:58 +0300

    Update hello.pas, 1 task is done

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
-﻿begin
-  Print('Привет, мир!');
+begin
+  var time := ReadInteger('Введите час');
+  Assert (time in 0..23);
+  case time of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+    23, 0..3: Print('Доброй ночи, мир!');
+  end;
 end.
